830d2ffb9f4e8a75fc841b8eb7a9fc5ed913c973
Pavel Pakhmutov <info@extroot.ru>
2025-02-24 21:34:30 +0000

    Add settings icon in tab bar & page template

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -1,3 +1,4 @@
+import 'package:cube_guide/pages/settings_page.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -10,8 +11,6 @@ import 'method_page.dart';
 
 class MenuPage extends StatefulWidget {
   final MenuEntry menuEntry;
-
-  // list of Cube objects
 
   // constructor
   const MenuPage({super.key, required this.menuEntry});
@@ -31,36 +30,47 @@ class _MenuPageState extends State<MenuPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('${widget.menuEntry.prefix}.title'.tr()),
-        ),
-        body: FutureBuilder(
-          future: DBHelper.getMenuGroupsByEntryId(widget.menuEntry.id),
-          builder: (BuildContext context, AsyncSnapshot<List<MenuGroup>> snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                  itemCount: snapshot.data!.length + (widget.menuEntry.show_description ? 1 : 0),
-                  // primary: true,
-                  // scrollDirection: Axis.vertical,
-                  // shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    if (widget.menuEntry.show_description && index == 0) {
-                      return Container(
-                        margin: const EdgeInsets.all(10),
-                        child: Text(
-                          '${widget.menuEntry.prefix}.description'.tr(),
-                          style: const TextStyle(fontSize: 16),
-                        ),
-                      );
-                    }
-                    MenuGroup group = snapshot.data![index - (widget.menuEntry.show_description ? 1 : 0)];
-                    return menuGroup(group);
-                  });
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-          },
-        ));
+      appBar: AppBar(
+        title: Text('${widget.menuEntry.prefix}.title'.tr()),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.settings),
+            tooltip: 'Open settings',
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SettingsPage()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: FutureBuilder(
+        future: DBHelper.getMenuGroupsByEntryId(widget.menuEntry.id),
+        builder: (BuildContext context, AsyncSnapshot<List<MenuGroup>> snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length + (widget.menuEntry.show_description ? 1 : 0),
+              // primary: true,
+              // scrollDirection: Axis.vertical,
+              // shrinkWrap: true,
+              itemBuilder: (context, index) {
+                if (widget.menuEntry.show_description && index == 0) {
+                  return Container(
+                    margin: const EdgeInsets.all(10),
+                    child: Text('${widget.menuEntry.prefix}.description'.tr(), style: const TextStyle(fontSize: 16)),
+                  );
+                }
+                MenuGroup group = snapshot.data![index - (widget.menuEntry.show_description ? 1 : 0)];
+                return menuGroup(group);
+              },
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
   }
 
   Widget menuGroup(group) {
@@ -116,13 +126,14 @@ class _MenuPageState extends State<MenuPage> {
 
   Widget menuGroupList(MenuGroup menuGroup) {
     return ListView.builder(
-        itemCount: menuGroup.menu_entries.length,
-        primary: false,
-        scrollDirection: Axis.vertical,
-        shrinkWrap: true,
-        itemBuilder: (context, index) {
-          return menuEntryRow(menuGroup.menu_entries[index], menuGroup);
-        });
+      itemCount: menuGroup.menu_entries.length,
+      primary: false,
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        return menuEntryRow(menuGroup.menu_entries[index], menuGroup);
+      },
+    );
   }
 
   Widget menuEntryRow(MenuEntry menuEntry, MenuGroup parentGroup) {
@@ -138,33 +149,36 @@ class _MenuPageState extends State<MenuPage> {
         // "${assetName}f2l/f2l0.svg",
         // height: 125,
         width: 120,
-        placeholderBuilder: (BuildContext context) =>
-            Container(padding: const EdgeInsets.all(30.0), child: const CircularProgressIndicator()),
+        placeholderBuilder:
+            (BuildContext context) =>
+                Container(padding: const EdgeInsets.all(30.0), child: const CircularProgressIndicator()),
       );
     }
     Widget column = Expanded(
-      child: Column(children: <Widget>[
-        Container(
-          margin: const EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 5),
-          width: double.infinity,
-          child: Text(
-            '${menuEntry.prefix}.title'.tr(),
-            style: const TextStyle(fontSize: 20),
-            textAlign: TextAlign.left,
-          ),
-        ),
-        if (menuEntry.show_description)
+      child: Column(
+        children: <Widget>[
           Container(
-            margin: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+            margin: const EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 5),
             width: double.infinity,
             child: Text(
-              '${menuEntry.prefix}.description'.tr(),
-              maxLines: 3,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(fontSize: 16),
+              '${menuEntry.prefix}.title'.tr(),
+              style: const TextStyle(fontSize: 20),
+              textAlign: TextAlign.left,
             ),
-          )
-      ]),
+          ),
+          if (menuEntry.show_description)
+            Container(
+              margin: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              width: double.infinity,
+              child: Text(
+                '${menuEntry.prefix}.description'.tr(),
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(fontSize: 16),
+              ),
+            ),
+        ],
+      ),
     );
 
     List<Widget> rowChildren = [image, column];
@@ -178,12 +192,7 @@ class _MenuPageState extends State<MenuPage> {
       child: Card(
         child: InkWell(
           splashColor: Colors.blue.withAlpha(30),
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              children: rowChildren,
-            ),
-          ),
+          child: Container(padding: const EdgeInsets.all(10), child: Row(children: rowChildren)),
           onTap: () {
             onTapMenuEntry(context, menuEntry, parentGroup);
           },
@@ -194,28 +203,28 @@ class _MenuPageState extends State<MenuPage> {
 
   Widget menuEntryCard(MenuEntry menuEntry, MenuGroup parentGroup) {
     return Card(
-        elevation: 3,
-        child: InkWell(
-            splashColor: Colors.blue.withAlpha(30),
-            child: Column(
-              children: <Widget>[
-                Container(
-                    margin: const EdgeInsets.only(top: 7, bottom: 3),
-                    child: Text(
-                      '${menuEntry.prefix}.title'.tr(),
-                      style: const TextStyle(fontSize: 20),
-                    )),
-                CubeSvg.cubeSvg(menuEntry.menu_picmode, menuEntry.menu_state, height: 125)
-              ],
+      elevation: 3,
+      child: InkWell(
+        splashColor: Colors.blue.withAlpha(30),
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(top: 7, bottom: 3),
+              child: Text('${menuEntry.prefix}.title'.tr(), style: const TextStyle(fontSize: 20)),
             ),
-            onTap: () {
-              onTapMenuEntry(context, menuEntry, parentGroup);
-            }));
+            CubeSvg.cubeSvg(menuEntry.menu_picmode, menuEntry.menu_state, height: 125),
+          ],
+        ),
+        onTap: () {
+          onTapMenuEntry(context, menuEntry, parentGroup);
+        },
+      ),
+    );
   }
 }
 
 void onTapMenuEntry(context, MenuEntry menuEntry, MenuGroup parentGroup) {
-// Start MethodPage
+  // Start MethodPage
   Navigator.push(
     context,
     MaterialPageRoute(
--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Settings Page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: const Text('Language'),
+            subtitle: const Text('English'),
+            onTap: () {
+              // Open language selection dialog
+            },
+          ),
+          ListTile(
+            title: const Text('Rate the app'),
+            onTap: () {
+              // Open the app store
+            },
+          ),
+          ListTile(
+            title: const Text('Privacy Policy'),
+            onTap: () {
+              // Open the privacy policy page
+            },
+          ),
+          ListTile(
+            title: const Text('Terms of Service'),
+            onTap: () {
+              // Open the terms of service page
+            },
+          ),
+        ],
+      ),
     );
   }
 }
